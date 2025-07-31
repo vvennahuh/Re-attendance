4835edcb7fb438714796f312834b88f117ecaf4c
vvennqhuh <vvennq@gmail.com>
2025-08-01 07:27:36 +0900

    コミット2

--- src/app/Http/Controllers/AdminAttendanceController.php
+++ src/app/Http/Controllers/AdminAttendanceController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Attendance;
+use App\Models\Rest;
+use App\Http\Requests\AdminDetailRequest;
 
 class AdminAttendanceController extends Controller
 {
-    //
+    // 勤怠一覧画面表示
+    public function index()
+    {
+        
+    }
+
+    // 勤怠詳細画面表示
+    public function show()
+    {
+
+    }
+
 }
--- src/app/Http/Controllers/AdminRequestController.php
+++ src/app/Http/Controllers/AdminRequestController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class AdminRequestController extends Controller
 {
-    //
+    // 勤怠一覧画面表示
+    public function index() {
+
+    }
+
+    // 勤怠詳細画面表示
+    public function show() {
+        
+    }//
 }
